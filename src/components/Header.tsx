8e987bcadb3f643a2fb8c2801d59f93da56f3721
scriptjumper <shaeendev@gmail.com>
2021-09-09 19:15:37 +0200

    Changed header component with styling and search input

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,47 @@
-import React from 'react'
+import * as React from 'react'
 
-const Header = () => {
-  return <div className="p-4">Header Component</div>
+import { Formik, Form, Field } from 'formik'
+import { useHistory } from 'react-router-dom'
+
+interface MyFormValues {
+  search: string
+}
+
+const Header: React.FC = () => {
+  const initialValues: MyFormValues = { search: '' }
+
+  let history = useHistory()
+
+  const handleSubmit = (search: string) => {
+    history.push(`/search/${search}`)
+  }
+
+  return (
+    <div className="text-black">
+      <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css" rel="stylesheet" />
+
+      <div className="w-screen flex flex-row items-center p-1 justify-between bg-gray-400 shadow-xs">
+        <div className="ml-8 text-lg text-gray-100 hidden md:flex">
+          People<span className="text-yellow-400">@</span>Star Wars
+        </div>
+
+        <Formik
+          initialValues={initialValues}
+          onSubmit={(values, actions) => {
+            handleSubmit(values.search)
+            actions.setSubmitting(false)
+          }}>
+          <Form className="w-screen md:w-1/5 h-10 bg-gray-200 cursor-pointer border border-gray-300 text-sm rounded-full flex bg-gray-100">
+            <label htmlFor="search"></label>
+
+            <Field className="flex-grow px-4 rounded-l-full rounded-r-full text-sm focus:outline-none" id="search" name="search" placeholder="Search" />
+
+            <button type="submit" className="fas fa-search m-3 mr-5 text-lg text-gray-700 w-4 h-4"></button>
+          </Form>
+        </Formik>
+      </div>
+    </div>
+  )
 }
 
 export default Header
