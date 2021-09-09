5ece80640a068cc642bbc7f2a228b09851980818
scriptjumper <shaeendev@gmail.com>
2021-09-09 17:39:55 +0200

    Adding app routes and linking in other components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import backGround from './bg-stars.jpeg'
 import { ApolloClient, InMemoryCache, ApolloProvider, useQuery } from '@apollo/client'
+import { PEOPLE_QUERY } from './components/Queries'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import People from './components/People'
+import Details from './components/Details'
 import Header from './components/Header'
+import Page from './components/Page'
 
 const client = new ApolloClient({
   uri: 'https://fsgm7.sse.codesandbox.io/',
@@ -10,6 +14,11 @@ const client = new ApolloClient({
 })
 
 export const App: React.FC = () => {
+  const usePeople = () => {
+    const { loading, error, data } = useQuery(PEOPLE_QUERY)
+    return { loading, error, data }
+  }
+
   return (
     <div className="text-gray-100 work-sans leading-normal text-base tracking-normal bg-cover " style={{ background: `url(${backGround}) repeat-y` }}>
       <ApolloProvider client={client}>
@@ -18,6 +27,9 @@ export const App: React.FC = () => {
             <section>
               <Header />
             </section>
+            <Route exact path="/" render={() => <People data={usePeople} currentPage={1} />} />
+            <Route exact path="/person/:name/:index" component={Details} />
+            <Route exact path="/page/:number" component={Page} />
           </div>
         </Router>
       </ApolloProvider>
