1300d0661f1f9b0fa15ca6551b4b1e938596855c
scriptjumper <shaeendev@gmail.com>
2021-09-09 18:12:40 +0200

    Added a clickable link to a persons card to route to the persons details screen

--- src/components/Person.tsx
+++ src/components/Person.tsx
@@ -9,7 +9,7 @@ const Person: React.FC<Props> = (Props) => {
 
   return (
     <div className={`w-full w-1 md:w-1/3 xl:w-1/5 p-5 flex flex-col items-center`}>
-      <a href="#">
+      <a href={`/person/${Props.name}/${Props.index + 1}`}>
         <img className="hover:grow hover:shadow-lg w-48 rounded-xl xl:w-48 w-60" alt={Props.name} src={imageURL} />
 
         <div className="pt-3 flex items-center justify-between">
