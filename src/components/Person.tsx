dbb54c35773c81b9d68de3d69070cc3d03914f01
scriptjumper <shaeendev@gmail.com>
2021-09-09 19:37:27 +0200

    Changed linking so page doesn't refresh

--- src/components/Person.tsx
+++ src/components/Person.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 interface Props {
   name: string
@@ -9,13 +10,14 @@ const Person: React.FC<Props> = (Props) => {
 
   return (
     <div className={`w-full w-1 md:w-1/3 xl:w-1/5 p-5 flex flex-col items-center`}>
-      <a href={`/person/${Props.name}/${Props.index + 1}`}>
-        <img className="hover:grow hover:shadow-lg w-48 rounded-xl xl:w-48 w-60" alt={Props.name} src={imageURL} />
-
-        <div className="pt-3 flex items-center justify-between">
-          <p className="">{Props.name}</p>
+      <Link className="flex items-center font-bold" to={`/person/${Props.name}/${Props.index + 1}`}>
+        <div>
+          <img className="hover:grow hover:shadow-lg w-48 rounded-xl xl:w-48 w-60" alt={Props.name} src={imageURL} />
+          <div className="pt-3 flex items-center justify-between">
+            <p className="">{Props.name}</p>
+          </div>
         </div>
-      </a>
+      </Link>
     </div>
   )
 }
