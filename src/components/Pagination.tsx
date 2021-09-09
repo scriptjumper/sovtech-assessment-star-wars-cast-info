0906293efb3beef704977062d3c20ffb89f6661d
scriptjumper <shaeendev@gmail.com>
2021-09-09 18:57:24 +0200

    Pagination hiding and showing buttons

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -3,25 +3,34 @@ import { Link } from 'react-router-dom'
 
 interface Props {
   currentPage: number
+  numberOfPages: number
 }
 
 const Pagination: React.FC<Props> = (Props) => {
   return (
     <div>
       <ul className="flex flex items-center justify-center text-gray-700 opacity-80">
-        <li className="mx-1 px-3 py-2 bg-yellow-400 hover:bg-yellow-500 rounded-lg hover:text-gray-100">
-          <Link className="flex items-center font-bold " to="">
-            Previous
-          </Link>
-        </li>
+        {Props.currentPage === 1 ? (
+          ''
+        ) : (
+          <li className="mx-1 px-3 py-2 bg-yellow-400 hover:bg-yellow-500 rounded-lg hover:text-gray-100">
+            <Link className="flex items-center font-bold " to={`/page/${Props.currentPage - 1}`}>
+              Previous
+            </Link>
+          </li>
+        )}
 
         <li className="mx-1 px-3 py-2 bg-yellow-400 rounded-lg">{Props.currentPage}</li>
 
-        <li className="mx-1 px-3 py-2 bg-yellow-400 hover:bg-yellow-500 rounded-lg hover:text-gray-100">
-          <Link className="flex items-center font-bold" to="">
-            Next
-          </Link>
-        </li>
+        {Props.currentPage === Props.numberOfPages ? (
+          ''
+        ) : (
+          <li className="mx-1 px-3 py-2 bg-yellow-400 hover:bg-yellow-500 rounded-lg hover:text-gray-100">
+            <Link className="flex items-center font-bold" to={`/page/${Props.currentPage + 1}`}>
+              Next
+            </Link>
+          </li>
+        )}
       </ul>
     </div>
   )
